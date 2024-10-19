cbd35d4853f64e4cd969ea8536fc09a126cefa03
dordos <pdhssf@gmail.com>
2024-10-19 13:07:36 +0900

    update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,7 +11,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require('mason-lspconfig').setup({
-        ensure_installed = { "lua_ls", "tsserver", "html" }
+        ensure_installed = { "lua_ls" }
       })
     end
   },
