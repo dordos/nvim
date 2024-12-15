497e50bc0dcabae5940a3199f497ab7456eac9fe
dordos <pdhssf@gmail.com>
2024-12-15 14:22:38 +0900

    update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,7 +11,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "ts_ls", "html", "eslint", "prettier" },
+				ensure_installed = { "lua_ls", "ts_ls", "html", "eslint" },
 			})
 		end,
 	},
@@ -21,11 +21,18 @@ return {
 			local lspconfig = require("lspconfig")
 
 			lspconfig.lua_ls.setup({})
-			lspconfig.ts_ls.setup({})
+			-- lspconfig.ts_ls.setup({})
+			lspconfig.html.setup({})
+			lspconfig.eslint.setup({})
 
 			keyMapper("K", vim.lsp.buf.hover)
 			keyMapper("gd", vim.lsp.buf.definition)
 			keyMapper("<leader>ca", vim.lsp.buf.code_action)
 		end,
+	},
+	{
+		"pmizio/typescript-tools.nvim",
+		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+		opts = {},
 	},
 }
