c574f9e65a698d354c2c8ac61d358ca7733b34ec
dordos <pdhssf@gmail.com>
2025-05-19 17:10:41 +0900

    prettier update

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,23 +1,25 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local conform = require("conform")
-		conform.setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				javascriptreact = { "prettier" },
-				typescriptreact = { "prettier" },
-			},
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_fallback = true,
-			},
-			prettier = {
-				single_quote = true,
-			},
-		})
-	end,
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
+    conform.setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+      formatters = {
+        prettier = {
+          prepend_args = { "--single-quote" },
+        },
+      },
+    })
+  end,
 }
