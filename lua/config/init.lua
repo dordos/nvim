97203d7ebf328ccde29d240be0ed1d83874330de
dordos <pdhssf@gmail.com>
2025-05-20 13:17:28 +0900

    clipboard update

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -10,8 +10,24 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
--- y로 시스템 클립보드 복사
-vim.opt.clipboard:append("unnamedplus")
+
+-- 클립보드 활성화
+vim.opt.clipboard = 'unnamedplus'
+-- 삭제 명령어가 클립보드에 영향을 주지 않도록 설정
+vim.keymap.set({ 'n', 'v', 'o' }, 'd', '"_d', { noremap = true })
+vim.keymap.set({ 'n', 'v', 'o' }, 'D', '"_D', { noremap = true })
+vim.keymap.set('n', 'dd', '"_dd', { noremap = true })
+
+-- 변경 명령어가 클립보드에 영향을 주지 않도록 설정
+vim.keymap.set({ 'n', 'v', 'o' }, 'c', '"_c', { noremap = true })
+vim.keymap.set({ 'n', 'v', 'o' }, 'C', '"_C', { noremap = true })
+vim.keymap.set('n', 'cc', '"_cc', { noremap = true })
+
+-- 기타 삭제 명령어가 클립보드에 영향을 주지 않도록 설정
+vim.keymap.set({ 'n', 'v' }, 'x', '"_x', { noremap = true })
+vim.keymap.set({ 'n', 'v' }, 'X', '"_X', { noremap = true })
+vim.keymap.set('n', 's', '"_s', { noremap = true })
+vim.keymap.set('n', 'S', '"_S', { noremap = true })
 
 require("config.globals")
 require("config.keymaps")
