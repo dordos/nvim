e588b93f29979a72dde4b7fc836716becb4498cc
dordos <pdhssf@gmail.com>
2025-06-14 21:22:26 +0900

    color nvim

--- lua/plugins/nvim-colorizer.lua
+++ lua/plugins/nvim-colorizer.lua
@@ -1,17 +1,14 @@
 return {
-  {
-    "NvChad/nvim-colorizer.lua",
-    opts = {
-      filetypes = { "*" },
-      user_default_options = {
-        names = false, -- color 이름 지원 ex: "red"
-        rgb_fn = true, -- rgb(), rgba(), hsl(), hsla() 지원
-        hsl_fn = true, -- hsl(), hsla() 지원
-        css = true, -- CSS 속성 안의 색상도 지원
-        tailwind = true, -- Tailwind 클래스도 하이라이트
-        mode = "background", -- 색상 미리보기를 배경으로 표시
-        virtualtext = "■", -- 또는 "■"처럼 사이드에 출력
-      }
-    }
-  }
+  "norcalli/nvim-colorizer.lua",
+  event = { "BufReadPost", "BufNewFile", "InsertEnter" },
+  opts = {
+    filetypes = { "*" },
+    user_default_options = {
+      mode = "background",
+      rgb_fn = true,
+      hsl_fn = true,
+      css = true,
+      css_fn = true
+    },
+  },
 }
