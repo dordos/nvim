26a46637ce6de2e8ee600a60cc869f359d7754be
dordos <pdhssf@gmail.com>
2025-07-16 23:27:13 +0900

    flash updqte

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -4,9 +4,9 @@ return {
   ---@type Flash.Config
   -- stylua: ignore
   keys = {
-    { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash" },
-    { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,     desc = "Toggle Flash Search" },
+    { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,   desc = "Flash" },
+    -- { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "<c-s>", mode = { "c" },           function() require("flash").toggle() end, desc = "Toggle Flash Search" },
   },
 
   config = function()
