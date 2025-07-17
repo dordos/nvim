bf7b2b977e1b15adff9022f044e5eccd6c8d519c
dordos <pdhssf@gmail.com>
2025-07-17 16:31:10 +0900

    rainbow-delimiters update

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -50,31 +50,32 @@ return {
   },
 
 
-  -- {
-  --   "HiPhish/rainbow-delimiters.nvim",
-  --   config = function()
-  --     local rainbow_delimiters = require("rainbow-delimiters")
-  --
-  --     vim.g.rainbow_delimiters = {
-  --       strategy = {
-  --         [""] = rainbow_delimiters.strategy["global"],
-  --         vim = rainbow_delimiters.strategy["local"],
-  --       },
-  --       query = {
-  --         [""] = "rainbow-delimiters",
-  --         lua = "rainbow-blocks",
-  --         tsx = "rainbow-parens"
-  --       },
-  --       highlight = {
-  --         "RainbowDelimiterRed",
-  --         "RainbowDelimiterYellow",
-  --         "RainbowDelimiterBlue",
-  --         "RainbowDelimiterOrange",
-  --         "RainbowDelimiterGreen",
-  --         "RainbowDelimiterViolet",
-  --         "RainbowDelimiterCyan",
-  --       },
-  --     }
-  --   end,
-  -- }
+  {
+    "HiPhish/rainbow-delimiters.nvim",
+    config = function()
+      vim.g.rainbow_delimiters = {
+        strategy = {
+          [''] = 'rainbow-delimiters.strategy.global',
+          vim = 'rainbow-delimiters.strategy.local',
+        },
+        query = {
+          [''] = 'rainbow-delimiters',
+          lua = 'rainbow-blocks',
+          tsx = "rainbow-parens",
+
+        },
+
+        highlight = {
+          "RainbowDelimiterRed",
+          "RainbowDelimiterYellow",
+          "RainbowDelimiterBlue",
+          "RainbowDelimiterOrange",
+          "RainbowDelimiterGreen",
+          "RainbowDelimiterViolet",
+          "RainbowDelimiterCyan",
+
+        },
+      }
+    end,
+  }
 }
