23b4745f1a87ae538b33cbfa9baa62a1b1435d49
dordos <pdhssf@gmail.com>
2025-08-11 15:23:11 +0900

    lualine update

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -6,6 +6,14 @@ return {
       options = {
         theme = "codedark"
       },
+      sections = {
+        lualine_c = {
+          {
+            'filename',
+            path = 1, -- 1: 부모 폴더, 2: 부모의 부모 폴더까지 표시
+          }
+        },
+      }
     })
   end
 }
