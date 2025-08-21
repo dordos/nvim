a12b7f38a7e39ca96c74fe3b67b5c88120364d64
dordos <pdhssf@gmail.com>
2025-08-21 20:58:19 +0900

    delete emmt key <C-space>

--- lua/plugins/emmet.lua
+++ lua/plugins/emmet.lua
@@ -2,12 +2,5 @@ return {
   "mattn/emmet-vim",
   config = function()
     vim.g.user_emmet_mode = 'i' -- insert 모드에서 활성화
-
-    vim.api.nvim_set_keymap(
-      'i',
-      '<C-Space>',
-      [[<C-\><C-n>:call emmet#expandAbbr('', '')<CR>]],
-      { noremap = true, silent = true }
-    )
   end,
 }
